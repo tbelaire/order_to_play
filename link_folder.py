cc512c1fa017a99131cf869c44cf1447192a252a
Theo Belaire <theo.belaire@gmail.com>
2015-10-03 16:30:15 -0400

    link can switch folders

--- link_folder.py
+++ link_folder.py
@@ -1,12 +1,16 @@
 import subprocess
 from subprocess import call
+import sys
 
-folder = "clips/rock"
+assert (len(sys.argv) == 2)
+#folder = "clips/rock"
+folder = sys.argv[1]
 
+audio_links = ['audioA.mp3', 'audioB.mp3', 'audioC.mp3', 'audioD.mp3', 'audioE.mp3']
+call(["rm"] + audio_links)
 p = subprocess.Popen(['ls', folder], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-for (song, target) in zip(p.stdout.readlines(), ['audioA.mp3', 'audioB.mp3', 'audioC.mp3', 'audioD.mp3', 'audioE.mp3']):
+for (song, target) in zip(p.stdout.readlines(), audio_links):
     song_path =  folder + "/" + song.strip()
-    print("ln -s " + "'" + song_path + "'" + " " + target)
     subprocess.call(['ln', '-s', song_path, target])
 
 
